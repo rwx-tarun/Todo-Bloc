d91f4b89f3b34e4af1bc72f9050dea27536df987
Tarun Singh <tarun.singh@reyahealthcare.com>
2024-11-19 01:56:54 +0530

    next

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Todo App',
         theme: ThemeData(
           brightness: Brightness.light,
